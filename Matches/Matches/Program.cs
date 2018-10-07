2011a8dfdb6d1149c201140742796cf7c26537bf
APrimikirio <a.d.primikirio@gmail.com>
2018-10-07 21:27:29 +0300

    add working version of the calculation method

--- Matches/Matches/Program.cs
+++ Matches/Matches/Program.cs
@@ -8,8 +8,43 @@ namespace Matches
 {
     class Program
     {
+        public static int Matches(int n)
+        {
+            if (n == 1) return 4;
+            if (n == 2) return 7;
+
+            int p = 1; int Squares = 2; int Matches = 7;
+
+            while (Squares < n)
+            {
+                Matches += 3; Squares = Squares + 1;
+                if (n == Squares) return Matches;
+
+                for (int i = 1; i <= p; i++)
+                {
+                    Matches = Matches + 2; Squares = Squares + 1;
+                    if (n == Squares) return Matches;
+                }
+
+                Matches += 3; Squares = Squares + 1;
+                if (n == Squares) return Matches;
+
+                for (int i = 1; i <= p; i++)
+                {
+                    Matches = Matches + 2;
+                    Squares = Squares + 1;
+                    if (n == Squares) return Matches;
+                }
+
+                p++;
+            }
+
+            return Matches;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Matches(4));
+            Console.ReadKey();
         }
     }
 }
